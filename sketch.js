1de719d70fc1ed2df7ae4a070a4f5c0430b15312
Nikhil982008 <76802695+Nikhil982008@users.noreply.github.com>
2021-03-30 19:39:14 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
 var backgroundIMG, sea;
 var fish, fishIMG;
-var obstacleGroup;
-var sharkIMG, fishHookIMG, plasticBottleIMG, plasticBagIMG;
+var obstacleGroup, obstacle, gameOver, restart;
+var sharkIMG, fishHookIMG, plasticBottleIMG, plasticBagIMG, gameOverIMG, restartIMG;
+var score;
 
 function preload() {
   backgroundIMG = loadImage("sea.png");
@@ -10,28 +15,45 @@ function preload() {
   fishHookIMG = loadImage("hook.png");
   plasticBottleIMG = loadImage("bottle.png")
   plasticBagIMG = loadImage("bag.png");
+  gameOverIMG = loadImage("gameOver.png");
+  restartIMG = loadImage("restart.png");
 }
 
 function setup() {
-  createCanvas(1350,650);
+  createCanvas(windowWidth,windowHeight);
+  score = 0;
   
-  sea = createSprite(400,250,400,700);
+  sea = createSprite(0,height-400,400,700);
   sea.addImage(backgroundIMG);
   sea.velocityX=-4;
   sea.position.x = width/2;
-  sea.scale=1.5;
+  sea.width=5;
+  sea.height=5;
 
   fish = createSprite(200,200,0,0);
   fish.addImage(fishIMG);
-  fish.scale=0.1;
+  fish.scale=0.05;
+
+  gameOver = createSprite(675, 300, 100, 20);
+  gameOver.addImage(gameOverIMG);
+
+  restart = createSprite(675,400,100,20);
+  restart.addImage(restartIMG);
+  restart.scale = 0.5;
 
   obstacleGroup = createGroup();
 }
 
 function draw() {
   background("blue");
-  spawnObstacles();
+  gameOver.visible = false;
+  restart.visible = false;
 
+  sea.velocityX=-4;;
+  
+  if(gameState === PLAY){
+  spawnObstacles();
+  score = score + Math.round(getFrameRate()/60);
   if(sea.position.x < 0){
     sea.position.x = width/2;
   }
@@ -39,40 +61,66 @@ function draw() {
   if (keyDown("LEFT_ARROW")){
     fish.position.x = fish.position.x - 5;
   }
-  if (keyDown("RIGHT_ARROW")){
+  if(keyDown("RIGHT_ARROW")){
     fish.position.x = fish.position.x + 5;
   }
-  if (keyDown("UP_ARROW")){
+  if(keyDown("UP_ARROW")){
     fish.position.y = fish.position.y - 5;
   }
-  if (keyDown("DOWN_ARROW")){
+  if(keyDown("DOWN_ARROW")){
     fish.position.y = fish.position.y + 5;
   }
 
-  drawSprites();
+  if(fish.isTouching(obstacleGroup)){
+    gameState = END;
+  }
+}
+
+if(gameState === END){
+  obstacle.velocityX = 0;
+  sea.velocityX = 0;
+  obstacleGroup.lifetime -= 1;
+  gameOver.visible = true;
+  restart.visible = true;
+
+  if(mousePressedOver(restart)) {
+    reset();
+  }
+}
+drawSprites();
+textSize(20);
+fill("black")
+text("Score: "+ score, 1200,50);
 }
 
 function spawnObstacles(){
   if(frameCount % 300 === 0){
-    var obstacle = createSprite(1350,300,10,40);
+    obstacle = createSprite(1350,300,10,40);
     obstacle.velocityX = -5;
 
     var rand = Math.round(random(1,4));
     switch (rand){
       case 1: obstacle.addImage(sharkIMG);
-      obstacle.scale = 2;
+      obstacle.scale = 1;
       break;
       case 2: obstacle.addImage(fishHookIMG);
-      obstacle.scale = 0.25;
+      obstacle.scale = 0.2;
       break;
       case 3: obstacle.addImage(plasticBottleIMG);
       obstacle.scale = 0.2;
       break;
       case 4: obstacle.addImage(plasticBagIMG);
-      obstacle.scale = 0.1;
+      obstacle.scale = 0.05;
       break;
     }
     obstacle.lifetime = 300;
     obstacleGroup.add(obstacle);
   }
+}
+
+function reset(){
+  gameState=PLAY;
+  gameOver.visible=false;
+  restart.visible=false;
+  obstacleGroup.destroyEach();
 }
